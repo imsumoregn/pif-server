731541625d12827f96c41734d058c9be78eba712
thuphuong1401 <thuphuongvu.amser@gmail.com>
2022-04-16 10:52:56 -0400

    log in done

--- modules/mentee/mentee.controller.js
+++ modules/mentee/mentee.controller.js
@@ -1,4 +1,6 @@
-const { Mentee } = require("../../models/index");
+const {
+  Mentee
+} = require("../../models/index");
 const bcrypt = require('bcrypt');
 const {
   validateCreateMentee,
@@ -6,17 +8,26 @@ const {
 } = require("../../helpers/validator.helper");
 
 const registerMentee = async (req, res) => {
-  const { error } = validateCreateMentee(req.body);
-  if(error) {
+  const {
+    error
+  } = validateCreateMentee(req.body);
+  if (error) {
     return res.status(400).send({
-      isError: true, 
+      isError: true,
       message: error.details[0].message.replace(/\"/g, "'"),
     });
   }
 
-  let mentee = await Mentee.findOne({ where: {email: req.body.email}});
+  let mentee = await Mentee.findOne({
+    where: {
+      email: req.body.email
+    }
+  });
   if (mentee) {
-    return res.status(400).send({ isError: true, message: "Email already exists!"});
+    return res.status(400).send({
+      isError: true,
+      message: "Email already exists!"
+    });
   }
 
   mentee = req.body;
@@ -52,7 +63,43 @@ const updateMenteeProfile = async () => {};
 
 const updateMenteeAvatar = async () => {};
 
-const menteeLogin = async () => {};
+const menteeLogin = async (req, res) => {
+  const {
+    email,
+    password
+  } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      isError: false,
+      message: "Missing email or password"
+    });
+  }
+
+  const mentee = await Mentee.findOne({
+    where: {
+      email: req.body.email
+    }
+  });
+  if (mentee) {
+    // check user password with hashed password stored in the database
+    const validPassword = await bcrypt.compare(password, mentee.password);
+    if (validPassword) {
+      res.status(200).json({
+        message: "Log in successful."
+      });
+    } else {
+      res.status(400).json({
+        error: "Invalid Password"
+      });
+    }
+  } else {
+    res.status(401).json({
+      error: "User does not exist"
+    });
+  }
+};
+
 
 const menteeTokenRefresh = async () => {};
 
